d3351fc2c908cf08c1e295f5edecd3db38b93b08
luishads <luishads@gmail.com>
2022-03-08 00:13:17 -0300

    add struct lst

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 22:17:48 by lamorim           #+#    #+#             */
-/*   Updated: 2022/03/07 22:19:30 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/03/07 23:37:41 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 # define PUSH_SWAP_H
 
 # include "ft_printf.h"
+
+typedef struct s_lst {
+	int				nbr;
+	struct s_lst	*next;
+}					t_lst;
 
 #endif
