3ab42d1fca64120bd336bf18b9df073c652408ac
luishads <luishads@gmail.com>
2022-03-08 22:01:08 -0300

    swap function rule

--- rules/swap.c
+++ rules/swap.c
@@ -6,8 +6,20 @@
 /*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 23:30:02 by lamorim           #+#    #+#             */
-/*   Updated: 2022/03/07 23:30:37 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/03/08 21:55:34 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	swap(t_lst **head)
+{
+	t_lst	*temp;
+
+	if (!*head)
+		return ;
+	temp = (*head)->next;
+	(*head)->next = temp->next;
+	temp->next = *head;
+	*head = temp;
+}
