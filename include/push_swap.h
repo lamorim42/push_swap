d83ac64494a996a65b884feacb02de0fcea27dd5
luishads <luishads@gmail.com>
2022-03-08 21:58:14 -0300

    t_lst top add

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 22:17:48 by lamorim           #+#    #+#             */
-/*   Updated: 2022/03/07 23:37:41 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/03/08 20:39:23 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,18 @@ typedef struct s_lst {
 	int				nbr;
 	struct s_lst	*next;
 }					t_lst;
+
+typedef struct s_stack {
+	t_lst			*top;
+	t_lst			*elem;
+	unsigned int	len;
+}					t_stack;
+
+//list functions
+t_lst	*ft_new_elem(int i);
+void	ft_push_front(t_lst **head, int i);
+void	ft_push_back(t_lst **last, int i);
+void	ft_clean_lst(t_lst **head);
+void	swap(t_lst **head);
 
 #endif
