0989de0354bfa7e9d2fcc09d90d0a4143d831cca
luishads <luishads@gmail.com>
2022-03-09 21:53:39 -0300

    New rule push add

---
+++ rules/push.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 15:04:17 by lamorim           #+#    #+#             */
+/*   Updated: 2022/03/09 20:16:01 by lamorim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	push(t_lst **from, t_lst **to, const char stk)
+{
+	if (!*from)
+		return ;
+	if (!*to)
+	{
+		(*to) = ft_new_elem((*from)->nbr);
+		ft_delone(from);
+	}
+	else
+	{
+		ft_push_front(to, (*from)->nbr);
+		ft_delone(from);
+	}
+	ft_printf("p%c\n", stk);
+}
