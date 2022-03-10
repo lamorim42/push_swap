710b0675cecc00c7fdda2047021a7d73f9163337
luishads <luishads@gmail.com>
2022-03-09 21:51:42 -0300

    adapted to print operation

--- rules/swap.c
+++ rules/swap.c
@@ -6,13 +6,13 @@
 /*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 23:30:02 by lamorim           #+#    #+#             */
-/*   Updated: 2022/03/08 21:55:34 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/03/09 20:15:23 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	swap(t_lst **head)
+void	swap(t_lst **head, const char stk)
 {
 	t_lst	*temp;
 
@@ -22,4 +22,13 @@ void	swap(t_lst **head)
 	(*head)->next = temp->next;
 	temp->next = *head;
 	*head = temp;
+	if (stk != 'n')
+		ft_printf("s%c\n", stk);
+}
+
+void	swap_bouth(t_lst **a, t_lst **b)
+{
+	swap(a, 'n');
+	swap(b, 'n');
+	ft_printf("ss\n");
 }
