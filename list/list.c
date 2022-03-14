1c92faa64b5326d5b42e60b82005668e26821524
luishads <luishads@gmail.com>
2022-03-14 15:52:06 -0300

    fix: norm bug
    add norm value to list functions

--- list/list.c
+++ list/list.c
@@ -6,39 +6,40 @@
 /*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 23:30:10 by lamorim           #+#    #+#             */
-/*   Updated: 2022/03/12 12:26:45 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/03/13 11:24:03 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_lst	*ft_new_elem(int i)
+t_lst	*ft_new_elem(int data, int norm)
 {
 	t_lst	*node;
 
 	node = malloc(sizeof(t_lst));
 	if (!node)
 		return (NULL);
-	node->nbr = i;
+	node->nbr = data;
+	node->norm = norm;
 	node->next = NULL;
 	return (node);
 }
 
-void	ft_push_front(t_lst **head, int i)
+void	ft_push_front(t_lst **head, int data, int norm)
 {
 	t_lst	*new;
 
-	new = ft_new_elem(i);
+	new = ft_new_elem(data, norm);
 	new->next = *head;
 	*head = new;
 }
 
-void	ft_push_back(t_lst **head, int i)
+void	ft_push_back(t_lst **head, int data, int norm)
 {
 	t_lst	*new;
 	t_lst	*temp;
 
-	new = ft_new_elem(i);
+	new = ft_new_elem(data, norm);
 	if (!*head)
 		*head = new;
 	else
