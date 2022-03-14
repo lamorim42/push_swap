72ce86da86591f9b9d7436b093c8237c984ba565
luishads <luishads@gmail.com>
2022-03-14 15:45:44 -0300

    refactor: add functions to header
    adding new functions to better organize source
    files

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 22:17:48 by lamorim           #+#    #+#             */
-/*   Updated: 2022/03/12 13:11:48 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/03/13 14:56:19 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 # include "ft_printf.h"
 
 # define MAX_INT 2147483647
+
+//Bool
+# define FALSE 0
+# define TRUE 1
 
 typedef struct s_lst {
 	int				nbr;
@@ -30,9 +34,9 @@ typedef struct s_stack {
 }					t_stack;
 
 //List functions
-t_lst		*ft_new_elem(int i);
-void		ft_push_front(t_lst **head, int i);
-void		ft_push_back(t_lst **last, int i);
+t_lst		*ft_new_elem(int data, int norm);
+void		ft_push_front(t_lst **head, int data, int norm);
+void		ft_push_back(t_lst **last, int data, int norm);
 t_lst		*ft_last_lst(t_lst *head);
 int			ft_is_empty(t_lst *lst);
 void		ft_clean_lst(t_lst **head);
@@ -58,5 +62,17 @@ long int	ft_long_atoi(const char *nptr);
 void		bubble_sort(t_lst **head);
 void		ft_normalize(t_lst **a);
 int			ft_can_put_in_lst(char *arg_v);
+
+//push_swap core
+void		radix_sort(t_lst **a, t_lst **b, int len);
+void		push_swap(t_stack *a, t_stack *b, int arg_c);
+
+//Small sort
+void		ft_chose_rules(t_lst **first, t_lst	**second, t_lst	**third, \
+			t_lst **head);
+void		sort_three(t_lst **head);
+void		ft_min_totop(t_lst **head, int min, char stk);
+void		sort_five(t_lst **head_a, t_lst **head_b, int min_ok);
+void		sort_six(t_lst **head_a, t_lst **head_b);
 
 #endif
