26e1df575a5c311954af3ba128394b461aa2c917
luishads <luishads@gmail.com>
2022-03-14 15:55:40 -0300

    fix: better functions calls

--- rules/push.c
+++ rules/push.c
@@ -6,7 +6,7 @@
 /*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 15:04:17 by lamorim           #+#    #+#             */
-/*   Updated: 2022/03/10 18:45:14 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/03/13 11:22:09 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ void	push(t_lst **from, t_lst **to, const char stk)
 		return ;
 	if (!*to)
 	{
-		(*to) = ft_new_elem((*from)->nbr);
+		(*to) = ft_new_elem((*from)->nbr, (*from)->norm);
 		ft_delone(from);
 	}
 	else
 	{
-		ft_push_front(to, (*from)->nbr);
+		ft_push_front(to, (*from)->nbr, (*from)->norm);
 		ft_delone(from);
 	}
 	ft_printf("p%c\n", stk);
