ff3b112c919bea06820059ac766da9f5b1384917
luishads <luishads@gmail.com>
2022-03-14 15:57:15 -0300

    style: put in norma

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,69 +6,18 @@
 /*   By: lamorim <lamorim@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 22:30:18 by lamorim           #+#    #+#             */
-/*   Updated: 2022/03/12 13:10:21 by lamorim          ###   ########.fr       */
+/*   Updated: 2022/03/13 18:31:41 by lamorim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-static void	ft_printf_lst(t_lst *lst)
-{
-	if (!lst)
-	{
-		ft_printf("Empty list!\n");
-		return ;
-	}
-	while (lst->next)
-	{
-		ft_printf("%d\n", lst->nbr);
-		lst = lst->next;
-	}
-	ft_printf("%d\n", lst->nbr);
-}
-
-static void	ft_printf_norm(t_lst *lst)
-{
-	if (!lst)
-	{
-		ft_printf("Empty list!\n");
-		return ;
-	}
-	while (lst)
-	{
-		ft_printf("%d\n", lst->norm);
-		lst = lst->next;
-	}
-}
-
-int	ft_can_put_in_lst(char *arg_v)
-{
-	const char	*nbr;
-	int			len;
-	int			i;
-
-	nbr = "-0123456789";
-	len = ft_strlen(arg_v);
-	if (!len)
-		return (1);
-	i = 0;
-	while (i < len)
-	{
-		if (!ft_strchr(nbr, arg_v[i]))
-			return (1);
-		i++;
-	}
-	if (ft_long_atoi(arg_v) > MAX_INT || ft_long_atoi(arg_v) < MIN_INT)
-		return (1);
-	return (0);
-}
 
 int	main(int argc, char **argv)
 {
 	t_stack	a;
 	t_stack	b;
 
-	if (argc <= 2)
+	if (argc < 2)
 		return (1);
 	ft_init_stack(&a, 'a');
 	ft_init_stack(&b, 'b');
@@ -79,13 +28,46 @@ int	main(int argc, char **argv)
 		ft_clean_lst(&(a.lst));
 		return (0);
 	}
-	ft_printf_lst(a.lst);
-	ft_printf("\n---------\n\n");
-	ft_normalize(&(a.lst));
-	ft_printf_norm(a.lst);
-	if (!ft_is_empty(a.lst))
-		ft_clean_lst(&(a.lst));
-	if (!ft_is_empty(b.lst))
-		ft_clean_lst(&(b.lst));
+	push_swap(&a, &b, argc);
 	return (0);
+}
+
+void	push_swap(t_stack *a, t_stack *b, int arg_c)
+{
+	ft_normalize(&(a->lst));
+	if (arg_c == 3 || arg_c == 4)
+		sort_three(&(a->lst));
+	else if (arg_c == 5 || arg_c == 6)
+		sort_five(&(a->lst), &(b->lst), FALSE);
+	else if (arg_c == 7)
+		sort_six(&(a->lst), &(b->lst));
+	else if (arg_c > 7)
+		radix_sort(&(a->lst), &(b->lst), (int)a->len);
+	if (!ft_is_empty(a->lst))
+		ft_clean_lst(&(a->lst));
+	if (!ft_is_empty(b->lst))
+		ft_clean_lst(&(b->lst));
+}
+
+void	radix_sort(t_lst **a, t_lst **b, int len)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (ft_is_sorted(a))
+	{
+		j = 0;
+		while (j < len)
+		{
+			if (((*a)->norm >> i) & 1)
+				rotate(a, 'a');
+			else
+				push(a, b, 'b');
+			j++;
+		}
+		while (!ft_is_empty(*b))
+			push(b, a, 'a');
+		i++;
+	}
 }
